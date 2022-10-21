5d9f78a6be9be2f3e1eee32047794e242512596f
aylindoganer <doganeraylinn@gmail.com>
2022-10-21 13:11:05 +0300

    created header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src=""/>
+            <h2>Meme Generator</h2>
+        </header>
+    )
+}
